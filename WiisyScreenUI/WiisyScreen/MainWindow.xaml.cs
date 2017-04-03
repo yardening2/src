f6b9fdd087c76d0862df512976cfef31210c3dee
0fer <ofer.shen@gmail.com>
2017-04-03 22:53:24 +0300

    Inital UI upload

---
+++ WiisyScreenUI/WiisyScreen/MainWindow.xaml.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Forms;
+using ScreenSaver;
+using winMacros;
+using System.Threading;
+
+
+namespace WiisyScreen
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //this.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
+            //this.MaxWidth = SystemParameters.MaximizedPrimaryScreenWidth;
+        }
+
+        private void Window_Activated(object sender, EventArgs e)
+        {
+            this.Topmost = true;
+        }
+
+        private void Window_Deactivated(object sender, EventArgs e)
+        {
+            this.Topmost = true;
+            this.Activate();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var desktopWorkingArea = System.Windows.SystemParameters.WorkArea;
+            this.Width = desktopWorkingArea.Width;
+            this.Height = desktopWorkingArea.Height;
+            this.Left = desktopWorkingArea.Right - this.Width;
+            this.Top = desktopWorkingArea.Bottom - this.Height;
+            
+        }
+
+        private void buttonCalibrate_Click(object sender, RoutedEventArgs e)
+        {
+            gridBoard.Visibility = gridMacros.Visibility = Visibility.Hidden;
+            gridCalibrate.Visibility = Visibility.Visible;
+            inkCanvasBoard.Opacity = 0;
+        }
+
+        private void buttonMacros_Click(object sender, RoutedEventArgs e)
+        {
+            gridBoard.Visibility = gridCalibrate.Visibility = Visibility.Hidden;
+            gridMacros.Visibility = Visibility.Visible;
+            inkCanvasBoard.Opacity = 0;
+        }
+
+        private void buttonBoard_Click(object sender, RoutedEventArgs e)
+        {
+            gridMacros.Visibility = gridCalibrate.Visibility = Visibility.Hidden;
+            gridBoard.Visibility = Visibility.Visible;
+            inkCanvasBoard.Opacity = 1;
+        }
+
+        private string chooseFolder()
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == System.Windows.Forms.DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    return fbd.SelectedPath;
+                }
+            }
+
+            return null;
+        }
+
+        private void ButtonSaveScreen_Click(object sender, RoutedEventArgs e)
+        {
+            string dirToSaveTo = chooseFolder();
+            System.Threading.Thread.Sleep(250);
+            if (dirToSaveTo != null)
+            {
+                ScreenSaver.ScreenSaver.SaveAsImage(dirToSaveTo);
+            }
+        }
+
+        private void buttonGeneratePDF_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlgImagesToPDF = new Microsoft.Win32.OpenFileDialog();
+            
+            dlgImagesToPDF.DefaultExt = ".png";
+            dlgImagesToPDF.Filter = "PNG Files (*.png)|*.png";
+            dlgImagesToPDF.Multiselect = true;
+
+            Nullable<bool> result = dlgImagesToPDF.ShowDialog();
+
+            if (result == true)
+            {
+                // Open document 
+                Microsoft.Win32.SaveFileDialog saveFileDialog = new Microsoft.Win32.SaveFileDialog();
+                saveFileDialog.DefaultExt = ".pdf";
+                saveFileDialog.Filter = "PDF File (*.pdf)|*.pdf";
+                Nullable<bool> saveResult = saveFileDialog.ShowDialog();
+                if (result == true)
+                {
+                    ScreenSaver.ScreenSaver.CreatePDF(saveFileDialog.FileName, dlgImagesToPDF.FileNames);
+                }
+            }
+        }
+
+        private void buttonEraser_Click(object sender, RoutedEventArgs e)
+        {
+            inkCanvasBoard.EditingMode = InkCanvasEditingMode.EraseByStroke;
+        }
+
+        private void buttonPencil_Click(object sender, RoutedEventArgs e)
+        {
+            inkCanvasBoard.EditingMode = InkCanvasEditingMode.Ink;
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void buttonWindows_Click(object sender, RoutedEventArgs e)
+        {
+            Macros.WindowsScreen();
+        }
+
+        private void buttonDesktop_Click(object sender, RoutedEventArgs e)
+        {
+            Macros.Desktop();
+            //todo
+            //show this window
+        }
+
+        private void buttonTskmgr_Click(object sender, RoutedEventArgs e)
+        {
+            Macros.Taskmgr();
+        }
+
+        private void buttonKeyboard_Click(object sender, RoutedEventArgs e)
+        {
+            new Thread(Macros.osk).Start();
+        }
+
+        private void buttonMaximaze_Click(object sender, RoutedEventArgs e)
+        {
+            Topmost = false;
+            Macros.LastWindowShow(Macros.ShowWindowCommands.ShowMaximized);
+            Topmost = true;
+        }
+
+        private void buttonShift0_Click(object sender, RoutedEventArgs e)
+        {
+            Topmost = false;
+            Macros.ShiftLastWindow(0);
+            Topmost = true;
+        }
+
+        private void buttonShift1_Click(object sender, RoutedEventArgs e)
+        {
+            Topmost = false;
+            Macros.ShiftLastWindow(1);
+            Topmost = true;
+
+        }
+
+        private void buttonShift2_Click(object sender, RoutedEventArgs e)
+        {
+            Topmost = false;
+            Macros.ShiftLastWindow(2);
+            Topmost = true;
+        }
+    }
+
+}
---
+++ winMacros/winMacros/Macros.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WindowsInput;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+
+namespace winMacros
+{
+    public static class Macros
+    {
+        public static void WindowsScreen()
+        {
+            InputSimulator.SimulateModifiedKeyStroke(VirtualKeyCode.LWIN, VirtualKeyCode.TAB);
+        }
+
+        public static void Desktop()
+        {
+            InputSimulator.SimulateModifiedKeyStroke(VirtualKeyCode.LWIN, VirtualKeyCode.VK_D);
+        }
+
+        public static void Taskmgr()
+        {
+            Process.Start("taskmgr");
+        }
+
+        public static void osk()
+        {
+            Process.Start("osk");
+        }
+
+        public static void ShiftLastWindow(int i_slot)
+        {
+            IntPtr lastWindowHandle = getLastWindow();
+
+            if (lastWindowHandle.Equals(GetDesktopWindow()) == false && IsWindowVisible(lastWindowHandle))
+            {
+                ShowWindow(lastWindowHandle, ShowWindowCommands.Normal);
+                MoveWindow(lastWindowHandle, 600 * i_slot, 0, 600, 600, true);
+                SetForegroundWindow(lastWindowHandle);
+            }
+        }
+
+        public static void LastWindowShow(ShowWindowCommands i_swc)
+        {
+            IntPtr lastWindowHandle = getLastWindow();
+
+            if (lastWindowHandle.Equals(GetDesktopWindow()) == false)
+            {
+                ShowWindow(lastWindowHandle, i_swc);
+                SetForegroundWindow(lastWindowHandle);
+            }
+        }
+
+        private static IntPtr getLastWindow()
+        {
+            IntPtr deskTopPtr = GetDesktopWindow();
+            IntPtr lastWindowHandle = GetWindow(Process.GetCurrentProcess().MainWindowHandle, (uint)GetWindow_Cmd.GW_HWNDNEXT);
+            while (true)
+            {
+                IntPtr temp = GetParent(lastWindowHandle);
+                if (temp.Equals(IntPtr.Zero)) break;
+                lastWindowHandle = temp;
+            }
+            return lastWindowHandle;
+        }
+
+
+
+
+
+        //user32.dll functions and enums
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern IntPtr GetWindow(IntPtr hWnd, uint uCmd);
+
+        private enum GetWindow_Cmd : uint
+        {
+            GW_HWNDFIRST = 0,
+            GW_HWNDLAST = 1,
+            GW_HWNDNEXT = 2,
+            GW_HWNDPREV = 3,
+            GW_OWNER = 4,
+            GW_CHILD = 5,
+            GW_ENABLEDPOPUP = 6
+        }
+
+        [DllImport("user32.dll", ExactSpelling = true, CharSet = CharSet.Auto)]
+        private static extern IntPtr GetParent(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern bool MoveWindow(IntPtr hWnd, int X, int Y, int nWidth, int nHeight, bool bRepaint);
+
+        [DllImport("user32.dll", SetLastError = false)]
+        private static extern IntPtr GetDesktopWindow();
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool ShowWindow(IntPtr hWnd, ShowWindowCommands nCmdShow);
+
+        public enum ShowWindowCommands : uint
+        {
+            Hide = 0,
+            Normal = 1,
+            ShowMinimized = 2,
+            Maximize = 3,
+            ShowMaximized = 3,
+            ShowNoActivate = 4,
+            Show = 5,
+            Minimize = 6,
+            ShowMinNoActive = 7,
+            ShowNA = 8,
+            Restore = 9,
+            ShowDefault = 10,
+            ForceMinimize = 11
+        }
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool IsWindowVisible(IntPtr hWnd);
+    }
+}
