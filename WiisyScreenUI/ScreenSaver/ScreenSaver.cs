ad0251f032a93e06f2406e82dd7b2db5c014e6fb
Orzuri <tzori.or@gmail.com>
2017-04-04 19:47:15 +0300

    Moving ScreenSaver.dll to the Solution

---
+++ WiisyScreenUI/ScreenSaver/ScreenSaver.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using iTextSharp;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System.Windows.Forms;
+
+
+namespace ScreenSaver
+{
+    static public class ScreenSaver
+    {
+        static public void SaveAsImage(string dirToSaveTo)
+        {
+            String[] filesInDirectory = Directory.GetFiles(dirToSaveTo);
+            
+
+            Bitmap printscreen = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+            Graphics graphics = Graphics.FromImage(printscreen as System.Drawing.Image);
+            graphics.CopyFromScreen(0, 0, 0, 0, printscreen.Size);
+
+            printscreen.Save(dirToSaveTo + "\\" + "screenshot" + filesInDirectory.Count() + ".png", ImageFormat.Png);
+        }
+
+        
+        static public void CreatePDF(string fileToCreate , string[] filesToGenerate)
+        {
+            //String[] filesInDirectory = Directory.GetFiles(dirToSaveTo);
+            Document document = new Document();
+            using (var stream = new FileStream(fileToCreate, FileMode.Create, FileAccess.Write, FileShare.None))
+            {
+                PdfWriter.GetInstance(document, stream);
+                document.Open();
+                foreach (var file in filesToGenerate)
+                {
+                    if (file.EndsWith("png"))
+                    {
+                        using (var imageStream = new FileStream(file, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+                        {
+                            var image = iTextSharp.text.Image.GetInstance(imageStream);
+                            image.ScaleToFit(document.PageSize.Width - (document.LeftMargin + document.RightMargin),
+                                document.PageSize.Height - (document.BottomMargin + document.TopMargin));
+                            document.Add(image);
+                        }
+                    }
+                }
+
+                document.Close();
+            }
+        }//CreatePDF
+    }
+}
