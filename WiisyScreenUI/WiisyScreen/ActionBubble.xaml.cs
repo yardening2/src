173defb10fea1ba91bfe4e360a2ac97e6b5833d3
orzuri <tzori.or@gmail.com>
2017-07-11 21:35:04 +0300

    Adding option to change the actionBubble image from outside the usercontrol

--- WiisyScreenUI/WiisyScreen/ActionBubble.xaml.cs
+++ WiisyScreenUI/WiisyScreen/ActionBubble.xaml.cs
@@ -1,16 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace WiisyScreen
@@ -24,5 +13,11 @@ namespace WiisyScreen
         {
             InitializeComponent();
         }
-    }
+
+        public System.Windows.Media.Brush FrontBrush
+        {
+            get { return front.Fill; }
+            set { front.Fill = value; }
+        }
+    }  
 }
